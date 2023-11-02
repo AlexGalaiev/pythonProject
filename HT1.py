456c6734a26bc6a1996b07bb9d94cf873228fbc1
Alex Galaiev <sashagalaievsecond@gmail.com>
2023-11-02 16:54:22 +0200

    1

--- HT1.py
+++ HT1.py
@@ -50,16 +50,43 @@
 # year = int(input("what year is it now:"))
 # print("this user is:", name, "age is", year-age)
 ##################
-print("task 7")
-cels_temperature = float(input("please input temerature in cels: "))
-print("temperature in faringets is:", (cels_temperature * (9/5)) + 32)
-##################
-print("task 8")
-far_temperature = float(input("please input temerature in faringeits: "))
-print("temperature in cels is:", (far_temperature - 32) * (5/9))
-##################
-print("task 9")
-side_1 = float(input("please input side 1 value: "))
-side_2 = float(input("please input side 2 value: "))
-side_3 = ((side_1**2)+(side_2**2))**(1/2)
-print("GIPOTENUZA is:", side_3)
+# print("task 7")
+# cels_temperature = float(input("please input temerature in cels: "))
+# print("temperature in faringets is:", (cels_temperature * (9/5)) + 32)
+# ##################
+# print("task 8")
+# far_temperature = float(input("please input temerature in faringeits: "))
+# print("temperature in cels is:", (far_temperature - 32) * (5/9))
+# ##################
+# print("task 9")
+# side_1 = float(input("please input side 1 value: "))
+# side_2 = float(input("please input side 2 value: "))
+# side_3 = ((side_1**2)+(side_2**2))**(1/2)
+# print("GIPOTENUZA is:", side_3)
+#####################
+# print("task 10")
+# children = int(input("number of children: "))
+# apples = int(input("number of apples:" ))
+# if apples > 10000:
+#     print("Too many apples")
+# elif children >10000:
+#     print("too many people")
+# else:
+#     print("apples every person get = ", apples // children)
+#     print("not used apples: ", apples % children)
+
+#####################
+# # print("task 11")
+# pen = int(input("number of pen: "))
+# pencil = int(input("number of pencils: "))
+# marker = int(input("number of markers: "))
+# price_of_pencil = 3
+# price_of_pen = 5
+# price_of_marker = 12
+# print("user bought all for price:", price_of_marker + price_of_pen + price_of_pencil)
+#########################
+print("task 12")
+minutes_inputted = int(input("how many minutes passed from 12am: "))
+print("watches will show- hours: minutes", minutes_inputted//60, ":", minutes_inputted%60 )
+#########################
+print("task 13")
